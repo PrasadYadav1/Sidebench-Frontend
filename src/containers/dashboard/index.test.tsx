43306fd4eb2effd0f0cdaedfb83cf0cd499eb0e3
Sai durga <saidurga.chirumamilla@technoidentity.com>
2022-11-23 19:40:34 +0530

    Make design changes; Integrate search and date filter;

--- src/containers/dashboard/index.test.tsx
+++ src/containers/dashboard/index.test.tsx
@@ -1,13 +1,14 @@
 import {
   act,
+  fireEvent,
   render,
   screen,
   waitFor,
   waitForElementToBeRemoved
 } from '@testing-library/react';
+import { AxiosResponse } from 'axios';
 import Dashboard from './index';
 import * as apiCalls from '../../utils/apis';
-import { AxiosResponse } from 'axios';
 import lookBooksData from '../../mock/lookBooksData';
 
 describe('Dashboard Component test', () => {
@@ -26,9 +27,7 @@ describe('Dashboard Component test', () => {
     expect(getByText('Finished Orders')).toBeInTheDocument();
     expect(getByText('Finished Orders')).toBeInTheDocument();
     expect(getByTestId('search')).toBeInTheDocument();
-    expect(getByTestId('action-test1')).toBeInTheDocument();
     expect(getByTestId('action-test2')).toBeInTheDocument();
-    expect(getByTestId('action-test3')).toBeInTheDocument();
   });
 
   describe('Lookbooks data on load', () => {
@@ -100,5 +99,173 @@ describe('Dashboard Component test', () => {
       const orders = screen.getAllByTestId('order-text1');
       expect(orders[0]).toBeInTheDocument();
     });
+  });
+});
+describe('Lookbooks data on search', () => {
+  it('Error popup should display when there is error in api call', async () => {
+    jest
+      .spyOn(apiCalls, 'getApi')
+      .mockImplementationOnce(
+        () =>
+          Promise.resolve({
+            data: {
+              data: {
+                rows: lookBooksData,
+                count: 3
+              }
+            }
+          }) as Promise<AxiosResponse<unknown>>
+      )
+      .mockImplementationOnce(() =>
+        Promise.reject(new Error('Something went wrong. Please contact admin.'))
+      );
+    render(<Dashboard />);
+
+    const searchInput = screen
+      .queryByTestId('lookbook-search')
+      ?.querySelector('input');
+    expect(searchInput).toBeInTheDocument();
+    if (searchInput) {
+      fireEvent.change(searchInput, { target: { value: 'abcdefgh' } });
+      await act(async () => {
+        // await new Promise((r) => setTimeout(r, 3500));
+      });
+      await waitFor(() => {
+        expect(
+          screen.queryByText('Something went wrong. Please contact admin.')
+        ).toBeInTheDocument();
+      });
+    }
+  });
+  it('Data should display on search api call success with more than 0 records', async () => {
+    jest
+      .spyOn(apiCalls, 'getApi')
+      .mockImplementationOnce(
+        () =>
+          Promise.resolve({
+            data: {
+              data: {
+                rows: lookBooksData,
+                count: 3
+              }
+            }
+          }) as Promise<AxiosResponse<unknown>>
+      )
+      .mockImplementationOnce(
+        () =>
+          Promise.resolve({
+            data: {
+              data: {
+                rows: lookBooksData,
+                count: 3
+              }
+            }
+          }) as Promise<AxiosResponse<unknown>>
+      );
+    render(<Dashboard />);
+
+    const searchInput = screen
+      .queryByTestId('lookbook-search')
+      ?.querySelector('input');
+    expect(searchInput).toBeInTheDocument();
+    if (searchInput) {
+      fireEvent.change(searchInput, { target: { value: 'abcdefgh' } });
+      await act(async () => {
+        // await new Promise((r) => setTimeout(r, 3500));
+      });
+      const orders = screen.getAllByTestId('order-text1');
+      expect(orders[0]).toBeInTheDocument();
+    }
+  });
+});
+describe('Lookbook data on date filter', () => {
+  const dateSelect = async () => {
+    const firstDay = screen.queryAllByText('1')[0];
+    expect(firstDay).toHaveAttribute('aria-disabled', 'false');
+    fireEvent.click(firstDay);
+    await act(async () => {
+      // await new Promise((r) => setTimeout(r, 3500));
+    });
+    fireEvent.click(firstDay);
+    await act(async () => {
+      // await new Promise((r) => setTimeout(r, 3500));
+    });
+  };
+
+  it('Error popup should display when there is error in api call', async () => {
+    jest
+      .spyOn(apiCalls, 'getApi')
+      .mockImplementationOnce(
+        () =>
+          Promise.resolve({
+            data: {
+              data: {
+                rows: {
+                  'To Do': [],
+                  'In Progress': [],
+                  Queued: []
+                },
+                count: 0
+              }
+            }
+          }) as Promise<AxiosResponse<unknown>>
+      )
+      .mockImplementationOnce(() =>
+        Promise.reject(new Error('Something went wrong. Please contact admin.'))
+      );
+    render(<Dashboard />);
+    const filterInput = screen.queryByPlaceholderText('Sort by due date');
+
+    expect(filterInput).toBeInTheDocument();
+    if (filterInput) {
+      fireEvent.click(filterInput);
+      await act(async () => {
+        // await new Promise((r) => setTimeout(r, 3500));
+      });
+      await dateSelect();
+      expect(
+        screen.queryByText('Something went wrong. Please contact admin.')
+      ).toBeInTheDocument();
+    }
+  });
+  it('Data should display on search api call success with more than 0 records', async () => {
+    jest
+      .spyOn(apiCalls, 'getApi')
+      .mockImplementationOnce(
+        () =>
+          Promise.resolve({
+            data: {
+              data: {
+                rows: lookBooksData,
+                count: 3
+              }
+            }
+          }) as Promise<AxiosResponse<unknown>>
+      )
+      .mockImplementationOnce(
+        () =>
+          Promise.resolve({
+            data: {
+              data: {
+                rows: lookBooksData,
+                count: 3
+              }
+            }
+          }) as Promise<AxiosResponse<unknown>>
+      );
+    render(<Dashboard />);
+
+    const filterInput = screen.queryByPlaceholderText('Sort by due date');
+
+    expect(filterInput).toBeInTheDocument();
+    if (filterInput) {
+      fireEvent.click(filterInput);
+      await act(async () => {
+        // await new Promise((r) => setTimeout(r, 3500));
+      });
+      await dateSelect();
+      const orders = screen.getAllByTestId('order-text1');
+      expect(orders[0]).toBeInTheDocument();
+    }
   });
 });
